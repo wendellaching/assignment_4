e85a3a384a5f79aa9c925085906fdeb635e30b80
wendellaching <vincent23>
2015-09-29 15:57:34 -0400

    grass color and foreground
    placed grass on foreground

--- challenge_1/sketch.js
+++ challenge_1/sketch.js
@@ -8,11 +8,14 @@ function setup() {
 function draw() {
 	// clear the background
 	background(100, 190, 250);
+	//fill for grass
+	fill(40,180,50);
+	rect(0,280,640,200);
 
-	// set a fill color
-	fill(255, 255, 255);
-
-	// draw the ellipse
-	var diameter = random(100, 200);
-	ellipse(320, 180, diameter, diameter);
+	// // set a fill color
+	// fill(255, 255, 255);
+	//
+	// // draw the ellipse
+	// var diameter = random(100, 200);
+	// ellipse(320, 180, diameter, diameter);
 }
