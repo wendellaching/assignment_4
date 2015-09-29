0b0367879237bc39d51990684e4e6f3c117ec611
wendellaching <vincent23>
2015-09-29 17:09:47 -0400

    flower stem looping

--- challenge_1/sketch.js
+++ challenge_1/sketch.js
@@ -1,7 +1,9 @@
 function setup() {
 	// create a place to draw
 	createCanvas(640, 480);
+	colorMode(RGB);
 	noStroke();
+	noLoop();
 }
 
 function draw() {
@@ -11,18 +13,27 @@ function draw() {
 	fill(40,180,50);
 	rect(0,280,640,200);
 	//random flowers
-	drawFlower(random(0,640), random(0,280),340, 150);
+	drawFlower(random(0,640), random(0,280),50);
+	drawFlower(random(0,640), random(0,280),40);
+	drawFlower(random(0,640), random(0,280),30);
+	drawFlower(random(0,640), random(0,280),20);
+	drawFlower(random(0,640), random(0,280),10);
+}
 
 function drawFlower(x,y,size) {
 	//stems of flower
-	fill(0,120,10);
-	rect(30, 20, 05, 110, 20);
+	var i = 10;
+	for (var i=0; i<10; i++){
+			fill(0,120,10);
+			rect(random(0,640), random(450,240), 05, 110,20);
 }
 
-	// // set a fill color
-	// fill(255, 255, 255);
-	//
-	// // draw the ellipse
-	// var diameter = random(100, 200);
-	// ellipse(320, 180, diameter, diameter);
+
+//
+// 	// // set a fill color
+// 	// fill(255, 255, 255);
+// 	//
+// 	// // draw the ellipse
+// 	// var diameter = random(100, 200);
+// 	// ellipse(320, 180, diameter, diameter);
 }
