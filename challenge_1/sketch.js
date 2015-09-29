0c6f7b7f99bf847c4bd3b0a81d47be0e1840cd62
wendellaching <vincent23>
2015-09-29 19:21:35 -0400

    redoing the "array" codes
    recopying

--- challenge_1/sketch.js
+++ challenge_1/sketch.js
@@ -1,79 +1,22 @@
-function setup() {
-	// create a place to draw
-	createCanvas(640, 480);
-	colorMode(RGB);
-	noStroke();
-	// noLoop();
-	var positionX = [];
-	var positionY = [];
+var xPositions = [];
+var yPositions = [];
 
+function setup () {
+		createCanvas(640,480);
+		noStroke();
+		noLoop();
+		colorMode(RGB);
+		for (var i = 0, i < 30; i++){
+				xPositions[i] = random(0,640);
+				yPositions[i] = random(200,450);
+		}
 }
-
 function draw() {
-	// clear the background
-	background(100, 190, 250);
-	//fill for grass
-	fill(40,180,50);
-	rect(0,280,640,200);
-	//random flowers
-	drawFlower(random(0,640), random(0,280),50);
-	drawFlower(random(0,640), random(0,280),40);
-	drawFlower(random(0,640), random(0,280),30);
-	drawFlower(random(0,640), random(0,280),20);
-	drawFlower(random(0,640), random(0,280),10);
+		background (100,190,250);
+		for (var i = 0; i < 10; i++){
+		xPositions[i] = xPositions[i] + random(-2, 2);
+		yPositions[i] = yPositions[i] + random(-2, 2);
+		//grass foreground
+		fill(40,180,50);
+		rect(0,280,640,200);
 }
-
-positionX = random(0,640);
-positionY = random(450,240);
-function drawFlower(x,y,size) {
-		//stem of flower
-		var i = 50;
-		for (var i=0; i<10; i++){
-				fill(0,120,10);
-				rect(positionX*i, positionY, 05, 110,20);
-		// var i = 10;
-		// for (var i=0; i<10; i++){
-		// 		fill(0,120,10);
-		// 		rect(random(0,640), random(450,240), 05, 110,20);
-		// first in ring
-		fill(255,255,255,80)
-		ellipse( positionX, positionY,size-25,size-25);
-		//second ring
-		fill(255,255,255,70)
-		ellipse( positionX, positionY,size-20,size-20);
-
-	}
-}
-
-
-
-
-	// //stems of flower
-	// var i = 10;
-	// for (var i=0; i<10; i++){
-	// 		fill(0,120,10);
-	// 		rect(random(0,640), random(450,240), 05, 110,20);
-	// 		//first ring
-	// 		fill(255,255,255,80)
-	// 		ellipse(random(0,640),random(450,240),size,size);
-	// 		//second ring
-	// 		fill(255,255,255,70)
-	// 		ellipse(random(0,640),random(450,240),size-10,size-10);
-	// 		//thired ring
-	// 		fill(255,255,255,60)
-	// 		ellipse(random(0,640),random(450,240),size-20,size-20);
-
-
-	//second ring
-//
-// }
-//
-//
-// //
-// // 	// // set a fill color
-// // 	// fill(255, 255, 255);
-// // 	//
-// // 	// // draw the ellipse
-// // 	// var diameter = random(100, 200);
-// // 	// ellipse(320, 180, diameter, diameter);
-// }
