e51b0644d84d7ed8757aacb8d1ec02a40c776822
wendellaching <vincent23>
2015-09-29 16:13:29 -0400

    fixed stem length

--- challenge_1/sketch.js
+++ challenge_1/sketch.js
@@ -2,7 +2,6 @@ function setup() {
 	// create a place to draw
 	createCanvas(640, 480);
 	noStroke();
-	noLoop();
 }
 
 function draw() {
@@ -11,6 +10,14 @@ function draw() {
 	//fill for grass
 	fill(40,180,50);
 	rect(0,280,640,200);
+	//random flowers
+	drawFlower(random(0,640), random(0,280),340, 150);
+
+function drawFlower(x,y,size) {
+	//stems of flower
+	fill(0,120,10);
+	rect(30, 20, 05, 110, 20);
+}
 
 	// // set a fill color
 	// fill(255, 255, 255);
