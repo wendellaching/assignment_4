fda2032e49b201c68934b2c8a2ec2aa43bdb3df5
wendellaching <vincent23>
2015-09-29 21:33:49 -0400

    black rectangle placement
    rect(40,50,250,380);

--- challenge_3/sketch.js
+++ challenge_3/sketch.js
@@ -1,13 +1,19 @@
+var myCanvas;
+
 function setup() {
 	// create a place to draw
 	createCanvas(640, 480);
+	background(90, 90, 110);
 	noStroke();
 	noLoop();
 }
 
 function draw() {
 	// clear the background
-	background(90, 90, 110);
+	fill(0, 0,0);
+	rect(40,50,250,380);
+	myCanvas = (320,480);
+
 
 
 }
