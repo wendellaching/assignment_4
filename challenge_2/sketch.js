ee03a6b6e4ed205e4c5420fdd859bd1cee5d8f60
wendellaching <vincent23>
2015-09-29 21:26:12 -0400

    canvas and background
    changed canvas and background

--- challenge_2/sketch.js
+++ challenge_2/sketch.js
@@ -4,7 +4,7 @@
 // The ball bounces when it hits the side of the canvas
 // and when it hits a mouse controlled paddle.
 
-// This challenge contains multiple syntax, runtime, and 
+// This challenge contains multiple syntax, runtime, and
 // logical errors. Find and fix the errors to make it work.
 
 
@@ -14,82 +14,65 @@ var ball = {
 	y: 100,
 	speedX: 5,
 	speedY: 5,
-	radius: 25
-};
-
+	radius: 25,
+}
 
 // paddle: a data object to hold info about the ball
 var paddle = {
 	x: 0,
-	y: 350
+	y: 350,
 	width: 300,
 	height: 30,
-};
+}
 
 
 // setup: called by p5 once at startup
-setup() {
+function setup() {
 	createCanvas(640, 480);
 	noStroke();
 }
 
-
-// draw: called by p5 repeatedly to draw the game
 function draw() {
-	
-	//////////////////////////////
-	// update
 
-	updateBall); 
+	updateBall);
 	paddle.x = mouseY;
-
-
-	//////////////////////////////
-	// draw
-
 	background(50, 50, 50);
 	circle(ball.x, ball.y, ball.radius * 2, ball.radius * 2);
 	square(paddle.x - paddle.width * 0.5, paddle.y, paddle.width, paddle.height);
-
 }
 
-
-// updateBall: move the ball, then detect and respond to collisions
-function updateBall() 
+function updateBall()
 {
 	// change position
 	ball.x = ball.x + ball.speedX;
 	ball.y = ball.y + ball.speedY;
-	
-
-	// don't let the ball go through the walls
 
 	// right
-	if (ball.x > width - ball.radius) {
-		ball.speedX = -ball.speedX;
+	if (ball.x > 640 - ball.radius) {
+			ball.speedX = - ball.speedX;
 	}
 
 	// bottom
-	if (ball.y > height - ball.radius) {
-		ball.speedY = ball.speedY;
+	if (ball.y > 480 - ball.radius) {
+			ball.speedY = - ball.speedY;
 	}
 
 	// left
 	if (ball.x < 0 + ball.radius) {
-		ball.speedX = -ball.speedX;
+			ball.speedX = - ball.speedX;
 	}
 
 	// top
 	if (ball.y < 0 + ball.radius) {
-		ball.speedY = -ball.speedY;
+			ball.speedY = - ball.speedY;
 	}
 
 	// check for a *collision* between the ball and the paddle
 	// look up the && operator
 
-	if (ball.x > (paddle.x - paddle.width * 0.5) && ball.x < (paddle.x + paddle.width * 0.5) && ball.y > paddle.y - ball.radius) {
+	if (ball.x > (paddle.x - paddle.480 * 0.5) && ball.x < (paddle.x + paddle.480 * 0.5) && ball.y > paddle.y - ball.radius) {
 		if (ball.speedY > 0) {
-			ball.speedY = -ball.speedY;
+			ball.speedY = - ball.speedY;
 		}
 	}
 }
--- challenge_3/sketch.js
+++ challenge_3/sketch.js
@@ -1,18 +1,13 @@
 function setup() {
 	// create a place to draw
-	createCanvas(640, 360);
+	createCanvas(640, 480);
 	noStroke();
 	noLoop();
 }
 
 function draw() {
 	// clear the background
-	background(150, 50, 150);
+	background(90, 90, 110);
 
-	// set a fill color
-	fill(255, 255, 255);
 
-	// draw the ellipse
-	var diameter = random(100, 200);
-	ellipse(320, 180, diameter, diameter);
 }
