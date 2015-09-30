bd776aa11cb31cfa94d05d7e923ecea106777cb0
wendellaching <vincent23>
2015-09-29 20:03:16 -0400

    redid the "array" codes and fixed syntax problem

--- challenge_1/sketch.js
+++ challenge_1/sketch.js
@@ -6,17 +6,25 @@ function setup () {
 		noStroke();
 		noLoop();
 		colorMode(RGB);
-		for (var i = 0, i < 30; i++){
+		background (100,190,250);
+		//grass
+		fill(40,180,50);
+		rect(0,280,640,200);
+
+		for (var i = 0; i < 30; i++){
 				xPositions[i] = random(0,640);
 				yPositions[i] = random(200,450);
 		}
 }
-function draw() {
-		background (100,190,250);
-		for (var i = 0; i < 10; i++){
-		xPositions[i] = xPositions[i] + random(-2, 2);
-		yPositions[i] = yPositions[i] + random(-2, 2);
-		//grass foreground
-		fill(40,180,50);
-		rect(0,280,640,200);
+
+function draw( x,y, size) {
+		var size = 30;
+		for (var i = 0; i < 30; i++){
+				//stems
+				fill(0,120,10);
+				rect(xPositions[i],yPositions[i], 05, 110, 20);
+				//first flower circle
+				fill(255,255,255,80);
+				ellipse(xPositions[i]+2, yPositions[i]+2, size-20,size-20);
+		}
 }
