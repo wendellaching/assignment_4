1ecc450e4f4e2730e055ada02f148a455ccf3b93
wendellaching <vincent23>
2015-09-29 22:02:46 -0400

    X made
    used 2 rounded rectangles

--- challenge_3/sketch.js
+++ challenge_3/sketch.js
@@ -14,6 +14,17 @@ function draw() {
 	rect(40,50,250,380);
 	myCanvas = (320,480);
 
+	// x
+	fill(200,50,50);
+	translate(width/2, height/2);
+	rotate(PI/4.0);
+	rect(30, 20, 25, 115, 20);
+	translate(width/-2, height/2);
+	rotate(PI/-2.0);
+	rect(150, 309, 25, 115, 20);
+
+	myCanvas.position(windowWidth/2, winMouseY+1);
+
 
 
 }
