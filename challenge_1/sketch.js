50a6046d0b33fc30b8814d0cd621384e8ccf575a
wendellaching <vincent23>
2015-09-29 20:30:39 -0400

    saved

--- challenge_1/sketch.js
+++ challenge_1/sketch.js
@@ -10,21 +10,34 @@ function setup () {
 		//grass
 		fill(40,180,50);
 		rect(0,280,640,200);
-
-		for (var i = 0; i < 30; i++){
+		for (var i = 0; i < 40; i++){
 				xPositions[i] = random(0,640);
 				yPositions[i] = random(200,450);
 		}
 }
 
 function draw( x,y, size) {
-		var size = 30;
-		for (var i = 0; i < 30; i++){
+		var size = 40;
+		for (var i = 0; i < 40; i++){
 				//stems
 				fill(0,120,10);
 				rect(xPositions[i],yPositions[i], 05, 110, 20);
 				//first flower circle
+				fill(255,255,255,100);
+				ellipse(xPositions[i]+2.5, yPositions[i]+2.5, size-25,size-25);
+				//second circle
+				fill(255,255,255,90);
+				ellipse(xPositions[i]+2.5, yPositions[i]+2.5, size-15,size-15);
+				//third circle
 				fill(255,255,255,80);
-				ellipse(xPositions[i]+2, yPositions[i]+2, size-20,size-20);
+				ellipse(xPositions[i]+2.5, yPositions[i]+2.5, size-05,size-05);
+				//fourth circle
+				fill(255,255,255,70);
+				ellipse(xPositions[i]+2.5, yPositions[i]+2.5, size+5,size+05);
+				// //fifth circle
+				// fill(255,255,255,60);
+				// ellipse(xPositions[i]+2.5, yPositions[i]+2.5, size+15,size+15);
+
+
 		}
 }
