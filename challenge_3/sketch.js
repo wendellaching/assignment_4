7ec90bedb46bd14b254b8e0108789a0da3b9b139
wendellaching <vincent23>
2015-10-01 17:58:11 -0400

    left and right walls

--- challenge_3/sketch.js
+++ challenge_3/sketch.js
@@ -12,17 +12,19 @@ function draw() {
 	// clear the background
 	fill(0, 0,0);
 	rect(40,50,225,380);
-	myCanvas = (320,480);
+	//walls
+	var leftWall = 0;
+	var rightWall = 240;
 	// x
-	fill(200,50,50);
-	translate(width/2, height/2);
-	rotate(PI/4.0);
-	rect(mouseX, 30, mouseY, 20, 25, 115, 20);
-	translate(width/-2, height/2);
-	rotate(PI/-2.0);
-	rect(mouseX, 150, mouseY, 309, 25, 115, 20);
-
-	myCanvas.position(windowWidth/2, winMouseY+1);
+	// fill(200,50,50);
+	// translate(width/2, height/2);
+	// rotate(PI/4.0);
+	// rect(mouseX, 30, mouseY, 20, 25, 115, 20);
+	// translate(width/-2, height/2);
+	// rotate(PI/-2.0);
+	// rect(mouseX, 150, mouseY, 309, 25, 115, 20);
+	//
+	// myCanvas.position(windowWidth/2, winMouseY+1);
 
 
 
