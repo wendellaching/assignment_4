7e1c417d83bab1bd01d31cbc3968a13c06c6dd50
wendellaching <vincent23>
2015-10-02 15:15:19 -0400

    saved

--- challenge_3/sketch.js
+++ challenge_3/sketch.js
@@ -3,13 +3,13 @@
 function setup() {
 	// create a place to draw
 	createCanvas(640, 480);
-	background(90, 90, 110);
 	noStroke();
 	// noLoop();
 }
 
 function draw() {
 	// clear the background
+	background(90, 90, 110);
 	fill(0, 0,0);
 	rect(40,50,225,380);
 
--- challenge_5/sketch.js
+++ challenge_5/sketch.js
@@ -2,8 +2,8 @@ function setup() {
 	// create a place to draw
 	// frameRate(1);
 	createCanvas(640, 480);
-	noStroke();
-	noLoop();
+	// noStroke();
+	//noLoop();
 }
 
 function draw() {
@@ -13,13 +13,13 @@ function draw() {
 	if (mouseIsPressed) {
 	  	stroke(255);
 	  	strokeWeight(10);
-	  	line(mouseX, mouseY, pmouseX, pmouseY);
+	  	line(mouseX, mouseY, pmouseX,pmouseY);
 	} else {
 		// frameRate(1);
 		fill (255,255,255);
 		ellipse (mouseX,mouseY, 50,50);
 		//inner circle
 		fill(0,100,255);
-		ellipse (mouseX, mouseY, 25,25);
+		ellipse(mouseX, mouseY, 25,25);
 	}
 }
