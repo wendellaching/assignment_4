dba6f990123b2a3af34c6775159893b13da920c4
wendellaching <vincent23>
2015-10-02 00:01:44 -0400

    changed canvas and colors

--- challenge_4/sketch.js
+++ challenge_4/sketch.js
@@ -7,47 +7,29 @@ function setup() {
 	for (var i = 0; i < 10; i++){
 		xPositions[i] = random(0, 640);
 		yPositions[i] = random(0, 480);
-	}
 	noStroke();
-	noLoop();
+	// noLoop();
+		}
 }
 
-function draw() {
-	// clear the background
+function draw(x,y,size) {
 	background(150,0,0);
 	fill(100, 250, 250);
-	for (var i = 0; i < 10; i++){
-		xPositions[i] = xPositions[i] + random(-2, 2);
-		yPositions[i] = yPositions[i] + random(-2, 2);
+		// var size = 20;
+		for (var i = 0; i < 10; i++){
+			yPositions[i] = yPositions[i] + random(-4, 4);
+			ellipse(xPositions[i]+ 2.5, yPositions[i]+2.5, 40, 40);
+			xPositions[i] = xPositions[i] + random(-10,10);
+			ellipse(xPositions[i]+70, yPositions[i] + 70, 30, 30);
+			xPositions[i] = xPositions[i]
+			ellipse(xPositions[i]+80, yPositions[i], 20, 20);
 
-		ellipse(xPositions[i], yPositions[i], 10, 10);
-	}
+		}
 }
 
 
 
 
 
-// // a simple p5 sketch that draws an ellipse
-// var xPositions = [];
-// var yPositions = [];
-//
-// function setup() {
-// 	createCanvas(640, 480);
-// 	for (var i = 0; i < 10; i++){
-// 		xPositions[i] = random(0, 640);
-// 		yPositions[i] = random(0, 480);
-//
-// 	}
-// }
-//
-// function draw() {
-// 	background(125, 125, 125);
-//
-// 	for (var i = 0; i < 10; i++){
-// 		xPositions[i] = xPositions[i] + random(-2, 2);
-// 		yPositions[i] = yPositions[i] + random(-2, 2);
-//
-// 		ellipse(xPositions[i], yPositions[i], 10, 10);
-// 	}
+
 // }
--- challenge_5/sketch.js
+++ challenge_5/sketch.js
@@ -1,16 +1,15 @@
 function setup() {
 	// create a place to draw
-	createCanvas(640, 360);
+	createCanvas(640, 480);
 	noStroke();
 	noLoop();
 }
 
 function draw() {
 	// clear the background
-	background(150, 150, 50);
-
-	// set a fill color
-	fill(255, 255, 255);
+	background(50, 10, 10);
+	// set fill color for inner circle
+	fill(0,100,255);
 
 	// draw the ellipse
 	var diameter = random(100, 200);
